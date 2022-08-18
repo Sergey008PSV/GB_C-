58c90db1344fd2e2c390f209a57356ba79d2839d
Sergey008PSV <111136982+Sergey008PSV@users.noreply.github.com>
2022-08-18 18:48:50 +0300

    1zada4a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
