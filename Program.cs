bf2839d6f6d6e523ab96b690a45b4a33df6f5c36
Sergey008PSV <111136982+Sergey008PSV@users.noreply.github.com>
2022-08-18 22:36:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
-﻿//1. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+//1. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
 
-Console.Write("Введите первое чиcло ");
+//Console.Write("Введите первое чиcло ");
+
+//int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Введите второе чиcло ");
+
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num1 > num2)
+
+//{
+//   Console.Write("большее число первое " +num1);
+//     }
+
+//else
+
+//{
+//    Console.Write("большее число второе " +num2);
+//}
+
+
+
+
+// 2. Напишите программу, которая принимает на вход 3 числа и выдает максимальное.
+
+ /* Console.Write("Введите первое чиcло ");
 
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -8,14 +33,61 @@ Console.Write("Введите второе чиcло ");
 
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
+Console.Write("Введите третье чиcло ");
 
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>=num2 && num1>=num3)
 {
-    Console.Write("большее число первое " +num1);
-     }
-
+    Console.Write("большее число : " +num1);
+}
 else
-
+if (num2>num1 && num2>=num3)
 {
-    Console.Write("большее число второе " +num2);
-    }
+    Console.Write("большее число : " +num2);
+}
+else
+if (num3>num1 && num3>num2)
+{
+    Console.Write("большее : " +num3);
+} 
+ */
+
+
+
+
+/* // Напишите программу, которая на вход принимает число и выдает, является ли число четным(делиться ли оно на два без остатка).
+
+Console.Write("Введите чиcло ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2.0 == 0.0)
+{
+    Console.Write("число четное");
+}
+else
+{
+    Console.Write("число нечетное");
+} */
+
+
+
+
+
+
+/* // 4. Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите чиcло больше 1 ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int x=1;
+ while (x <= num)
+{
+if (x % 2 != 1)
+{
+Console.Write(x + " " );
+}
+x++;
+} */
