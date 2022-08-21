8fcb75c6c9a43eca67714c3936139254bbe508a6
Sergey008PSV <111136982+Sergey008PSV@users.noreply.github.com>
2022-08-21 23:13:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -204,3 +204,42 @@ void PrintArry(int[] coll){
 int[] arry = new int[cube+1];
 Cube(arry);
 PrintArry(arry);
+
+
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/* int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
+ */
+
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/* 
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+ */
